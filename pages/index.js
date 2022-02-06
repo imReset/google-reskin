07b2ced7f195e2d67284929f61d4cabbb9577cfd
imReset <fffffreinds101@gmail.com>
2022-02-05 17:57:26 -0800

    add the searchbar

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import Avatar from "../components/Avatar";
+import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import { SearchIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -22,12 +26,25 @@ export default function Home() {
           <p className="link">Images</p>
 
           {/* Icon */}
+          <ViewGridIcon className="h-10 w-10 p-2 rounded-full hover:bg-gray-100 cursor-pointer" />
 
-          {/* Avatar */}
+          <Avatar url={"https://bit.ly/3LgNQii"} />
         </div>
       </header>
 
       {/* Body */}
+      <form>
+        <Image
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2f%2FGoogle_2015_logo.svg%2F1200px-Google_2015_logo.svg.png&f=1&nofb=1"
+          height={100}
+          width={300}
+        />
+        <div className="flex w-full mt-5 hover:shadow-lg focus-within:shadow-lg max-w-md rounded-full border border-gray-200 px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl">
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input type="text" className="flex-grow focus:outline-none" />
+          <MicrophoneIcon className="h-5" />
+        </div>
+      </form>
 
       {/* Footer */}
     </div>
