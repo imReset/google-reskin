4aa75c7a2741b36544b30cbcfd85f0dedc2fdbe4
imReset <fffffreinds101@gmail.com>
2022-02-06 20:18:17 -0800

    mae the search bar responsive

--- components/Header.js
+++ components/Header.js
@@ -1,34 +1,55 @@
-import { MicrophoneIcon, XIcon } from "@heroicons/react/solid";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useRef } from "react";
+import Avatar from "../components/Avatar";
+import HeaderOptions from "./HeaderOptions";
 
 function Header() {
   const router = useRouter();
   const searchInputRef = useRef(null);
 
-  return (
-    <header>
-      <Image
-        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2f%2FGoogle_2015_logo.svg%2F1200px-Google_2015_logo.svg.png&f=1&nofb=1"
-        height={40}
-        width={120}
-        onClick={() => router.push("/")}
-        className="cursor-pointer"
-      />
-      <form className="flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg mx-w-3xl items0center">
-        <input
-          ref={searchInputRef}
-          className="flex-grow w-full focus:outline-none"
-          type="text"
-        />
-        <XIcon
-          className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-150 transform hover:scale-125"
-          onClick={() => (searchInputRef.current.value = "")}
-        />
+  const search = (e) => {
+    e.preventDefault();
 
-        <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2" />
-      </form>
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F2%2F2f%2FGoogle_2015_logo.svg%2F1200px-Google_2015_logo.svg.png&f=1&nofb=1"
+          height={40}
+          width={120}
+          onClick={() => router.push("/")}
+          className="cursor-pointer"
+        />
+        <form className="flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg mx-w-3xl items-center">
+          <input
+            ref={searchInputRef}
+            className="flex-grow w-full focus:outline-none"
+            type="text"
+          />
+          <XIcon
+            className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-150 transform hover:scale-125"
+            onClick={() => (searchInputRef.current.value = "")}
+          />
+
+          <MicrophoneIcon className="mr-3 h-6 hidden sm:inline-flex text-blue-500 border-l-2 pl-4 border-gray-300" />
+          <SearchIcon className="h-6 text-blue-500 hidden sm:inline-flex" />
+          <button hidden type="submit" onClick={search}>
+            Search
+          </button>
+        </form>
+        <Avatar className="ml-auto" url={"https://bit.ly/3LgNQii"} />
+      </div>
+
+      <HeaderOptions />
     </header>
   );
 }
